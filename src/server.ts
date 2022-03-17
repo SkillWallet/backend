29db3ee887996119234abe3f250ab6c53f6741d8
mladen <mladenig2@gmail.com>
2022-03-17 22:30:40 +0200

    removed threaddb

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,6 @@ application.app.listen(PORT, async () => {
   console.log(process.env.MUMBAI_RPC_PROVIDER);
   console.log(process.env.SKILL_WALLET_ADDRESS);
   await connect(process.env.MONGODB_CONNECTION_STRING);
-  logger.info("ThreadDB initialized");
+  logger.info("MongodDB initialized");
   logger.info("SkillWallet API is listening on port " + PORT);
 });
