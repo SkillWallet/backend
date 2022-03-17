563e29fe1dfbde42f00f353101428983077ef19f
mladen <mladenig2@gmail.com>
2022-03-17 12:23:45 +0200

    unused functionality

--- src/contracts/distributedTown.contracts.ts
+++ src/contracts/distributedTown.contracts.ts
@@ -12,17 +12,6 @@ export class DistributedTownContracts {
     }
   }
 
-  public static async getMetadata(tokenID: string) {
-    try {
-      const contract = distributedTownContract();
-      const url = await contract.uri(tokenID);
-      console.log(url);
-      return url;
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
   public static async isDiToNativeCommunity(address: string) {
     try {
       const contract = distributedTownContract();
@@ -33,62 +22,4 @@ export class DistributedTownContracts {
     }
   }
 
-  // TODO: update
-  public static async createCommunity(
-    _url: string,
-    _ownerId: number,
-    _ownerCredits: number,
-    _name: string,
-    _template: number,
-    _positionalValue1: number,
-    _positionalValue2: number,
-    _positionalValue3: number
-  ) {
-    const distributedTownContractInst = distributedTownContract();
-
-    distributedTownContractInst.on(
-      'CommunityCreated',
-      (newCommunityAddress) => {
-        console.log('Community created!');
-        console.log(newCommunityAddress);
-      },
-    );
-    try {
-      let result = await distributedTownContractInst.createCommunity(
-        _url,
-        _ownerId,
-        _ownerCredits,
-        _name,
-        _template,
-        _positionalValue1,
-        _positionalValue2,
-        _positionalValue3
-      );
-
-      console.log(result);
-      return result;
-    } catch (err) {
-      console.log(err);
-      return;
-    }
-  };
-
-
-  public static async setPartnersRegistry(
-    address: string
-  ) {
-    const distributedTownContractInst = distributedTownContract();
-
-    try {
-      let result = await distributedTownContractInst.setPartnersRegistryAddress(
-        address
-      );
-
-      console.log(result);
-      return result;
-    } catch (err) {
-      console.log(err);
-      return;
-    }
-  };
 }
