4954f9bca5217b1b22a0cc28592ca6608f1296be
migrenaa <mimonova13@gmail.com>
2022-03-21 16:32:24 +0200

    no mongo docker compose

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ import { container } from "./inversify.config";
 
 import { App } from "./app";
 import { LoggerService } from "./services/logger.service";
-import { connect } from 'mongoose';
 
 // initialize configuration
 dotenv.config();
@@ -16,7 +15,7 @@ const logger = container.get<LoggerService>(LoggerService);
 application.app.listen(PORT, async () => {
   console.log(process.env.MUMBAI_RPC_PROVIDER);
   console.log(process.env.SKILL_WALLET_ADDRESS);
-  await connect(process.env.MONGODB_CONNECTION_STRING);
+  // await connect(process.env.MONGODB_CONNECTION_STRING);
   logger.info("ThreadDB initialized");
   logger.info("SkillWallet API is listening on port " + PORT);
 });
