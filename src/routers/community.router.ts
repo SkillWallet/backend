8faea0c337bc0b74503ff3e4856c42dbaaeb14b1
mladen <mladenig2@gmail.com>
2022-04-05 22:14:49 +0300

    commnity router swagger

--- src/routers/community.router.ts
+++ src/routers/community.router.ts
@@ -12,13 +12,128 @@ export class CommunityRouter {
 
   private init(): void {
     // GET
+
+  /**
+   * @swagger
+   * /api/community/{communityAddress}/skillwallet:
+   *   get:
+   *     description: Gets SkillWallets per community.
+   *     parameters:
+   *     - in: path
+   *       name: communityAddress
+   *       schema:
+   *         type: string
+   *       required: true
+   *     - in: query
+   *       name: coreTeamMembers
+   *       schema:
+   *         type: boolean
+   *       required: true
+   *     responses:
+   *       200:
+   *         description: Success
+   *       500:
+   *         description: Something went wrong, please try again later.
+   */
     this._router.get('/:communityAddress/skillwallet', this.communityController.get);
+  /**
+   * @swagger
+   * /api/community/{communityAddress}/coreTeamMembers:
+   *   get:
+   *     description: Gets core team members' names.
+   *     parameters:
+   *     - in: path
+   *       name: communityAddress
+   *       schema:
+   *         type: string
+   *       required: true
+   *     responses:
+   *       200:
+   *         description: Success
+   *       500:
+   *         description: Something went wrong, please try again later.
+   */
     this._router.get('/:communityAddress/coreTeamMembers', this.communityController.getCoreTeamMemberNames);
+  /**
+   * @swagger
+   * /api/community/key/{key}:
+   *   get:
+   *     description: Gets community by Partner Agreement key.
+   *     parameters:
+   *     - in: path
+   *       name: key
+   *       schema:
+   *         type: string
+   *       required: true
+   *     responses:
+   *       200:
+   *         description: Success
+   *       500:
+   *         description: Something went wrong, please try again later.
+   */
     this._router.get("/key/:key", this.communityController.getCommunityByPartnerAgreementKey);
+  /**
+   * @swagger
+   * /api/community/{communityAddress}/key:
+   *   get:
+   *     description: Gets Partner Agreement by community.
+   *     parameters:
+   *     - in: path
+   *       name: communityAddress
+   *       schema:
+   *         type: string
+   *       required: true
+   *     responses:
+   *       200:
+   *         description: Success
+   *       500:
+   *         description: Something went wrong, please try again later.
+   */
     this._router.get("/:communityAddress/key", this.communityController.getPAByCommunity);
 
     // POST
+
+  /**
+   * @swagger
+   * /api/community/{communityAddress}/coreTeamMembers:
+   *   post:
+   *     description: Inserts core team member.
+   *     parameters:
+   *     - in: path
+   *       name: communityAddress
+   *       schema:
+   *         type: string
+   *       required: true
+   *     requestBody:
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type:
+   *               object
+   *     responses:
+   *       200:
+   *         description: Success
+   *       500:
+   *         description: Something went wrong, please try again later.
+   */
     this._router.post('/:communityAddress/coreTeamMembers', this.communityController.insertCoreTeamMemberName);
+  /**
+   * @swagger
+   * /api/community/key:
+   *   post:
+   *     description: Posts Partner Agreement.
+   *     requestBody:
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type:
+   *               object
+   *     responses:
+   *       200:
+   *         description: Success
+   *       500:
+   *         description: Something went wrong, please try again later.
+   */
     this._router.post("/key", this.communityController.postPartnerAgreement)
   }
 
