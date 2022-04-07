3f7fc89ea94ab81dc6cdd32280e8d8827595c87e
migrenaa <mimonova13@gmail.com>
2022-04-07 17:52:05 +0200

    swagger endpoints hidden

--- src/routers/community.router.ts
+++ src/routers/community.router.ts
@@ -54,86 +54,86 @@ export class CommunityRouter {
    *         description: Something went wrong, please try again later.
    */
     this._router.get('/:communityAddress/coreTeamMembers', this.communityController.getCoreTeamMemberNames);
-  /**
-   * @swagger
-   * /api/community/key/{key}:
-   *   get:
-   *     description: Gets community by Partner Agreement key.
-   *     parameters:
-   *     - in: path
-   *       name: key
-   *       schema:
-   *         type: string
-   *       required: true
-   *     responses:
-   *       200:
-   *         description: Success
-   *       500:
-   *         description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/community/key/{key}:
+  //  *   get:
+  //  *     description: Gets community by Partner Agreement key.
+  //  *     parameters:
+  //  *     - in: path
+  //  *       name: key
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *     responses:
+  //  *       200:
+  //  *         description: Success
+  //  *       500:
+  //  *         description: Something went wrong, please try again later.
+  //  */
     this._router.get("/key/:key", this.communityController.getCommunityByPartnerAgreementKey);
-  /**
-   * @swagger
-   * /api/community/{communityAddress}/key:
-   *   get:
-   *     description: Gets Partner Agreement by community.
-   *     parameters:
-   *     - in: path
-   *       name: communityAddress
-   *       schema:
-   *         type: string
-   *       required: true
-   *     responses:
-   *       200:
-   *         description: Success
-   *       500:
-   *         description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/community/{communityAddress}/key:
+  //  *   get:
+  //  *     description: Gets Partner Agreement by community.
+  //  *     parameters:
+  //  *     - in: path
+  //  *       name: communityAddress
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *     responses:
+  //  *       200:
+  //  *         description: Success
+  //  *       500:
+  //  *         description: Something went wrong, please try again later.
+  //  */
     this._router.get("/:communityAddress/key", this.communityController.getPAByCommunity);
 
     // POST
 
-  /**
-   * @swagger
-   * /api/community/{communityAddress}/coreTeamMembers:
-   *   post:
-   *     description: Inserts core team member.
-   *     parameters:
-   *     - in: path
-   *       name: communityAddress
-   *       schema:
-   *         type: string
-   *       required: true
-   *     requestBody:
-   *       content:
-   *         application/json:
-   *           schema:
-   *             type:
-   *               object
-   *     responses:
-   *       200:
-   *         description: Success
-   *       500:
-   *         description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/community/{communityAddress}/coreTeamMembers:
+  //  *   post:
+  //  *     description: Inserts core team member.
+  //  *     parameters:
+  //  *     - in: path
+  //  *       name: communityAddress
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *     requestBody:
+  //  *       content:
+  //  *         application/json:
+  //  *           schema:
+  //  *             type:
+  //  *               object
+  //  *     responses:
+  //  *       200:
+  //  *         description: Success
+  //  *       500:
+  //  *         description: Something went wrong, please try again later.
+  //  */
     this._router.post('/:communityAddress/coreTeamMembers', this.communityController.insertCoreTeamMemberName);
-  /**
-   * @swagger
-   * /api/community/key:
-   *   post:
-   *     description: Posts Partner Agreement.
-   *     requestBody:
-   *       content:
-   *         application/json:
-   *           schema:
-   *             type:
-   *               object
-   *     responses:
-   *       200:
-   *         description: Success
-   *       500:
-   *         description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/community/key:
+  //  *   post:
+  //  *     description: Posts Partner Agreement.
+  //  *     requestBody:
+  //  *       content:
+  //  *         application/json:
+  //  *           schema:
+  //  *             type:
+  //  *               object
+  //  *     responses:
+  //  *       200:
+  //  *         description: Success
+  //  *       500:
+  //  *         description: Something went wrong, please try again later.
+  //  */
     this._router.post("/key", this.communityController.postPartnerAgreement)
   }
 
--- src/routers/skillWallet.router.ts
+++ src/routers/skillWallet.router.ts
@@ -12,77 +12,77 @@ export class SkillWalletRouter {
 
   private init(): void {
     // Activation & Validation
-  /**
-   * @swagger
-   * /api/skillWallet/{skillWalletId}/activate:
-   *   post:
-   *     description: Activates SkillWallet.
-   *     parameters:
-   *     - in: path
-   *       name: skillWalletId
-   *       schema:
-   *         type: string
-   *       required: true
-   *     requestBody:
-   *       content:
-   *         application/json:
-   *           schema:
-   *             type:
-   *               object
-   *       responses:
-   *         200:
-   *           description: Success
-   *         500:
-   *           description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/skillWallet/{skillWalletId}/activate:
+  //  *   post:
+  //  *     description: Activates SkillWallet.
+  //  *     parameters:
+  //  *     - in: path
+  //  *       name: skillWalletId
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *     requestBody:
+  //  *       content:
+  //  *         application/json:
+  //  *           schema:
+  //  *             type:
+  //  *               object
+  //  *       responses:
+  //  *         200:
+  //  *           description: Success
+  //  *         500:
+  //  *           description: Something went wrong, please try again later.
+  //  */
     this._router.post("/:skillWalletId/activate", this.skillWalletController.activateSkillWallet);
-  /**
-   * @swagger
-   * /api/skillWallet/{skillWalletId}/pubKey:
-   *   post:
-   *     description: Adds public key to SkillWallet.
-   *     parameters:
-   *     - in: path
-   *       name: skillWalletId
-   *       schema:
-   *         type: string
-   *       required: true
-   *     requestBody:
-   *       content:
-   *         application/json:
-   *           schema:
-   *             type:
-   *               object
-   *       responses:
-   *         200:
-   *           description: Success
-   *         500:
-   *           description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/skillWallet/{skillWalletId}/pubKey:
+  //  *   post:
+  //  *     description: Adds public key to SkillWallet.
+  //  *     parameters:
+  //  *     - in: path
+  //  *       name: skillWalletId
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *     requestBody:
+  //  *       content:
+  //  *         application/json:
+  //  *           schema:
+  //  *             type:
+  //  *               object
+  //  *       responses:
+  //  *         200:
+  //  *           description: Success
+  //  *         500:
+  //  *           description: Something went wrong, please try again later.
+  //  */
     this._router.post("/:skillWalletId/pubKey", this.skillWalletController.addPubKeyToSkillWallet);
-  /**
-   * @swagger
-   * /api/skillWallet/{skillWalletId}/validate:
-   *   post:
-   *     description: Requests SkillWallet validation.
-   *     parameters:
-   *     - in: path
-   *       name: skillWalletId
-   *       schema:
-   *         type: string
-   *       required: true
-   *     requestBody:
-   *       content:
-   *         application/json:
-   *           schema:
-   *             type:
-   *               object
-   *       responses:
-   *         200:
-   *           description: Success
-   *         500:
-   *           description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/skillWallet/{skillWalletId}/validate:
+  //  *   post:
+  //  *     description: Requests SkillWallet validation.
+  //  *     parameters:
+  //  *     - in: path
+  //  *       name: skillWalletId
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *     requestBody:
+  //  *       content:
+  //  *         application/json:
+  //  *           schema:
+  //  *             type:
+  //  *               object
+  //  *       responses:
+  //  *         200:
+  //  *           description: Success
+  //  *         500:
+  //  *           description: Something went wrong, please try again later.
+  //  */
     this._router.post("/:skillWalletId/validate", this.skillWalletController.validate);
   /**
    * @swagger
@@ -122,41 +122,41 @@ export class SkillWalletRouter {
    *           description: Something went wrong, please try again later.
    */
     this._router.get('/', this.skillWalletController.get);
-  /**
-   * @swagger
-   * /api/skillWallet/{skillWalletId}/interactions:
-   *   get:
-   *     description: Gets interaction NFTs.
-   *     parameters:
-   *     - in: path
-   *       name: skillWalletId
-   *       schema:
-   *         type: string
-   *       required: true
-   *       responses:
-   *         200:
-   *           description: Success
-   *         500:
-   *           description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/skillWallet/{skillWalletId}/interactions:
+  //  *   get:
+  //  *     description: Gets interaction NFTs.
+  //  *     parameters:
+  //  *     - in: path
+  //  *       name: skillWalletId
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *       responses:
+  //  *         200:
+  //  *           description: Success
+  //  *         500:
+  //  *           description: Something went wrong, please try again later.
+  //  */
     this._router.get("/:skillWalletId/interactions", this.skillWalletController.getInteractions);
-  /**
-   * @swagger
-   * /api/skillWallet/{skillWalletId}/events:
-   *   get:
-   *     description: Gets SkillWallet events.
-   *     parameters:
-   *     - in: path
-   *       name: skillWalletId
-   *       schema:
-   *         type: string
-   *       required: true
-   *       responses:
-   *         200:
-   *           description: Success
-   *         500:
-   *           description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/skillWallet/{skillWalletId}/events:
+  //  *   get:
+  //  *     description: Gets SkillWallet events.
+  //  *     parameters:
+  //  *     - in: path
+  //  *       name: skillWalletId
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *       responses:
+  //  *         200:
+  //  *           description: Success
+  //  *         500:
+  //  *           description: Something went wrong, please try again later.
+  //  */
     this._router.get("/:skillWalletId/events", this.skillWalletController.getEvents);
   /**
    * @swagger
@@ -199,122 +199,122 @@ export class SkillWalletRouter {
    *           description: Something went wrong, please try again later.
    */
     this._router.get("/:skillWalletId/tasks/:taskId", this.skillWalletController.getTaskById);
-  /**
-   * @swagger
-   * /api/skillWallet/{skillWalletId}/badges:
-   *   get:
-   *     description: Gets SkillWallet badges.
-   *     parameters:
-   *     - in: path
-   *       name: skillWalletId
-   *       schema:
-   *         type: string
-   *       required: true
-   *       responses:
-   *         200:
-   *           description: Success
-   *         500:
-   *           description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/skillWallet/{skillWalletId}/badges:
+  //  *   get:
+  //  *     description: Gets SkillWallet badges.
+  //  *     parameters:
+  //  *     - in: path
+  //  *       name: skillWalletId
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *       responses:
+  //  *         200:
+  //  *           description: Success
+  //  *         500:
+  //  *           description: Something went wrong, please try again later.
+  //  */
     this._router.get("/:skillWalletId/badges", this.skillWalletController.getBadges);
-  /**
-   * @swagger
-   * /api/skillWallet/{skillWalletId}/community/{communityAddress}/membershipID:
-   *   get:
-   *     description: Gets membership details.
-   *     parameters:
-   *     - in: path
-   *       name: skillWalletId
-   *       schema:
-   *         type: string
-   *       required: true
-   *     - in: path
-   *       name: communityAddress
-   *       schema:
-   *         type: string
-   *       required: true
-   *       responses:
-   *         200:
-   *           description: Success
-   *         500:
-   *           description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/skillWallet/{skillWalletId}/community/{communityAddress}/membershipID:
+  //  *   get:
+  //  *     description: Gets membership details.
+  //  *     parameters:
+  //  *     - in: path
+  //  *       name: skillWalletId
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *     - in: path
+  //  *       name: communityAddress
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *       responses:
+  //  *         200:
+  //  *           description: Success
+  //  *         500:
+  //  *           description: Something went wrong, please try again later.
+  //  */
     this._router.get("/:skillWalletId/community/:communityAddress/membershipID", this.skillWalletController.getMembershipID);
     
     // Nonces
-  /**
-   * @swagger
-   * /api/skillWallet/{skillWalletId}/nonces:
-   *   get:
-   *     description: Gets nonces for validation.
-   *     parameters:
-   *     - in: path
-   *       name: skillWalletId
-   *       schema:
-   *         type: string
-   *       required: true
-   *     - in: query
-   *       name: action
-   *       schema:
-   *         type: string
-   *       required: true
-   *       responses:
-   *         200:
-   *           description: Success
-   *         500:
-   *           description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/skillWallet/{skillWalletId}/nonces:
+  //  *   get:
+  //  *     description: Gets nonces for validation.
+  //  *     parameters:
+  //  *     - in: path
+  //  *       name: skillWalletId
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *     - in: query
+  //  *       name: action
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *       responses:
+  //  *         200:
+  //  *           description: Success
+  //  *         500:
+  //  *           description: Something went wrong, please try again later.
+  //  */
     this._router.get("/:skillWalletId/nonces", this.skillWalletController.getNonceForValidation);
-  /**
-   * @swagger
-   * /api/skillWallet/{skillWalletId}/nonces:
-   *   delete:
-   *     description: Invalidates nonce.
-   *     parameters:
-   *     - in: path
-   *       name: skillWalletId
-   *       schema:
-   *         type: string
-   *       required: true
-   *     - in: query
-   *       name: nonce
-   *       schema:
-   *         type: string
-   *       required: true
-   *     - in: query
-   *       name: action
-   *       schema:
-   *         type: string
-   *       required: true
-   *       responses:
-   *         200:
-   *           description: Success
-   *         500:
-   *           description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/skillWallet/{skillWalletId}/nonces:
+  //  *   delete:
+  //  *     description: Invalidates nonce.
+  //  *     parameters:
+  //  *     - in: path
+  //  *       name: skillWalletId
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *     - in: query
+  //  *       name: nonce
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *     - in: query
+  //  *       name: action
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *       responses:
+  //  *         200:
+  //  *           description: Success
+  //  *         500:
+  //  *           description: Something went wrong, please try again later.
+  //  */
     this._router.delete("/:skillWalletId/nonces", this.skillWalletController.deleteNonce);
-  /**
-   * @swagger
-   * /api/skillWallet/nonces:
-   *   post:
-   *     description: Generates nonce.
-   *     parameters:
-   *     - in: path
-   *       name: skillWalletId
-   *       schema:
-   *         type: string
-   *       required: true
-   *     - in: query
-   *       name: action
-   *       schema:
-   *         type: string
-   *       required: true
-   *       responses:
-   *         200:
-   *           description: Success
-   *         500:
-   *           description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/skillWallet/nonces:
+  //  *   post:
+  //  *     description: Generates nonce.
+  //  *     parameters:
+  //  *     - in: path
+  //  *       name: skillWalletId
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *     - in: query
+  //  *       name: action
+  //  *       schema:
+  //  *         type: string
+  //  *       required: true
+  //  *       responses:
+  //  *         200:
+  //  *           description: Success
+  //  *         500:
+  //  *           description: Something went wrong, please try again later.
+  //  */
     this._router.post('/:skillWalletId/nonces', this.skillWalletController.generateNonce);
 
     // Config
@@ -330,17 +330,17 @@ export class SkillWalletRouter {
    *         description: Something went wrong, please try again later.
    */
     this._router.get("/config", this.skillWalletController.getConfig);
-  /**
-   * @swagger
-   * /api/skillWallet/access:
-   *   post:
-   *     description: Authenticates Partner App.
-   *     responses:
-   *       200:
-   *         description: Success
-   *       500:
-   *         description: Something went wrong, please try again later.
-   */
+  // /**
+  //  * @swagger
+  //  * /api/skillWallet/access:
+  //  *   post:
+  //  *     description: Authenticates Partner App.
+  //  *     responses:
+  //  *       200:
+  //  *         description: Success
+  //  *       500:
+  //  *         description: Something went wrong, please try again later.
+  //  */
     this._router.post("/access", this.skillWalletController.authenticatePartnersApp);
 
   }
