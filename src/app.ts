84a093e71197705364a383d8d5f683c511fc9996
mladen <mladenig2@gmail.com>
2022-04-18 22:46:55 +0300

    ddos prevention

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,27 @@ import {
   SkillWalletRouter,
   CommunityRouter,
 } from "./routers";
+const rateLimit = require('express-rate-limit');
+const slowDown = require("express-slow-down");
+
+//  apply to all requests
 const cookieParser = require("cookie-parser");
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 var cors = require('cors');
 require('dotenv').config()
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+	standardHeaders: true,
+	legacyHeaders: false,
+});
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000,
+  delayAfter: 100,
+  delayMs: 500
+});
 
 const swaggerDefinition = {
   definition: {
@@ -69,6 +85,9 @@ export class App {
 
 
     this._app.use(cors());
+    this._app.use(limiter);
+    this._app.use(speedLimiter);
+
     //Initialize app routes
     this._initRoutes();
 
